0db1202611f1b0676b5565702a35ff3e4fd0cd58
Saki Shinoda <shinodasaki@gmail.com>
2017-06-17 00:35:31 +0100

    Experiment with click-based functionality

--- ui/main.py
+++ ui/main.py
@@ -8,56 +8,55 @@ class Application(tk.Frame):
         self.createWidgets()
         self.showImage()
         self.marker_dict = dict()
-
+        self.deleteMode = False
 
     def createWidgets(self):
 
         self.canvas = tk.Canvas(self, width=256, height=256, bd=5)
-        self.canvas.grid(row=0, column=1, columnspan=2)
+        self.canvas.grid(row=0, column=1, columnspan=3)
+
         self.canvas.bind('<Button-1>', self.addLabel)
-        self.canvas.bind('<Button-2>', self.editLabel)
+
+        # Mark as Maybe with right click
+        self.canvas.bind('<Button-2>', self.editLabel)  # for Mac
         self.canvas.bind('<Button-3>', self.editLabel)
 
-        self.quitButton = tk.Button(self, text='Quit', command=self.quit)
-        self.quitButton.grid(row=1, column=2)
+        # Buttons
+        self.deleteButton = tk.Button(self, text='Delete Mode', command=self.deleteMode)
+        self.deleteButton.grid(row=1, column=1)
         self.saveButton = tk.Button(self, text='Save')
-        self.saveButton.grid(row=1, column=1)
+        self.saveButton.grid(row=1, column=2)
+        self.quitButton = tk.Button(self, text='Quit', command=self.quit)
+        self.quitButton.grid(row=1, column=3)
 
-        # self.option_add('*tearOff', False)
-        # self.editMenu = tk.Menu(self)
-        # self.editMenu.add_command(label='Mark as Maybe')
-        # self.editMenu.add_command(label='Delete label', command=self.deleteMarker)
+        self.option_add('*tearOff', False)
+        self.editMenu = tk.Menu(self)
+        self.editMenu.add_command(label='Mark as Maybe', command=self.markAsMaybe)
+        self.editMenu.add_command(label='Delete label', command=self.deleteMarker)
 
         # Set up label listbox
         self.labelBox = tk.Listbox(self)
-        self.labelBox.grid(row=2, columnspan=2)
+        self.labelBox.grid(row=2, columnspan=3)
 
+    def deleteMode(self):
+        self.deleteMode = True if self.deleteMode is True else False
 
     def showImage(self):
         photo = ImageTk.PhotoImage(Image.open('sample.jpeg'))
         self.canvas.create_image(0, 0, anchor='nw', image=photo)
         self.canvas.current_image = photo
-    #
-    # def selectLabel(self):
-    #     selected = self.labelBox.curselection()
-    #     not_selected = set([i for i in range(self.labelBox.size())]) - set(selected)
-    #
-    #     for ix in selected:
-    #         marker = self.marker_list[ix]
-    #         self.canvas.itemconfig(marker, fill='yellow')
 
     def editLabel(self, event):
-        canvas = event.widget
-        x = canvas.canvasx(event.x)
-        y = canvas.canvasy(event.y)
-        marker = canvas.find_closest(x, y)
-        self.canvas.itemconfig(marker, fill='red')
+        # marker = self.canvas.gettags(tk.CURRENT)
+        self.canvas.itemconfig(tk.CURRENT, fill='red')
 
-        self.labelBox.delete(self.marker_dict[marker[0]])
-        self.labelBox.insert(self.marker_dict[marker[0]], '({},{}) MAYBE'.format(x, y))
-        # self.editMenu.post(event.x_root, event.y_root)
+        # self.labelBox.delete(self.marker_dict[marker[0]])
+        # self.labelBox.insert(self.marker_dict[marker[0]], '({},{}) MAYBE'.format(x, y))
+        self.editMenu.post(event.x_root, event.y_root)
 
-    # def deleteMarker(self, event):
+    def markAsMaybe(self):
+        rad = 5
+        self.canvas.addtag_overlapping(tk.CURRENT)
 
 
     def addLabel(self, event):
