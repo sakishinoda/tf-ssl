bb3359f8c3041c958712263db9845ca5b632ea28
Saki Shinoda <shinodasaki@gmail.com>
2017-06-17 02:24:26 +0100

    Complete radio button mode approach

--- ui/main.py
+++ ui/main.py
@@ -5,80 +5,101 @@ class Application(tk.Frame):
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
         self.grid(padx=10, pady=10)
+        self.currentMode = tk.IntVar()
+        self.currentMode.set(2)  # initialize
         self.createWidgets()
         self.showImage()
         self.marker_dict = dict()
-        self.currentMode = 0
+
+    def activate(self):
+        print(self.currentMode.get())
+
 
     def createWidgets(self):
 
-        self.canvas = tk.Canvas(self, width=256, height=256, bd=5)
-        self.canvas.grid(row=0, column=1, columnspan=3)
+        # Row 1
+        # Mode radio buttons
+        self.modeBox = tk.LabelFrame(self, text='Mode', width=256, labelanchor='n')
+        self.modeBox.grid(row=1, column=1)
 
+        MODES = [
+            ("Label", 2),
+            ("Delete", 0),
+            ("Maybe", 1)
+        ]
+        for text, mode in MODES:
+            b = tk.Radiobutton(self.modeBox, text=text,
+                               variable=self.currentMode, value=mode, indicatoron=0, command=self.activate)
+            b.grid(row=0, column=mode)
+
+        # Row 2 separator
+        separator = tk.Frame(self, height=5)
+        separator.grid(row=2, column=1)
+
+        # Row 3 canvas
+        self.canvas = tk.Canvas(self, width=256, height=256)
+        self.canvas.grid(row=3, column=1)
         self.canvas.bind('<Button-1>', self.editLabel)
 
-        # Mode buttons
-        self.modes = tk.LabelFrame(self, text='Mode', padx=5, pady=5)
-        self.modeButtons = []
-        for i, mode in enumerate(['Label', 'Delete', 'Maybe']):
-            self.modeButtons[i] = tk.Button(self.modes, text=mode, command=self.activateMode(i))
-        self.modes.grid(row=0, column=2)
 
-        # # Buttons
-        # self.deleteButton = tk.Button(self, text='Delete Mode', command=self.deleteMode)
-        # self.deleteButton.grid(row=1, column=1)
-        # self.saveButton = tk.Button(self, text='Save')
-        # self.saveButton.grid(row=1, column=2)
-        # self.quitButton = tk.Button(self, text='Quit', command=self.quit)
-        # self.quitButton.grid(row=1, column=3)
-
-        # # Set up label listbox
-        # self.labelBox = tk.Listbox(self)
-        # self.labelBox.grid(row=2, columnspan=3)
-
+        # Set up label listbox
+        self.coordTitle = tk.Label(self, text='Labels')
+        self.coordTitle.grid(row=4, column=1)
+        self.coordList = tk.Listbox(self, width=30)
+        self.coordList.grid(row=5, column=1)
 
     def showImage(self):
         photo = ImageTk.PhotoImage(Image.open('sample.jpeg'))
         self.canvas.create_image(0, 0, anchor='nw', image=photo)
         self.canvas.current_image = photo
 
-    def activateMode(self, mode):
-        for button in self.modeButtons:
-            button.config(relief=tk.RAISED)
-        self.modeButtons[mode].config(relief=tk.SUNKEN)
-        self.currentMode = mode
-
-
     def editLabel(self, event):
-        # marker = self.canvas.gettags(tk.CURRENT)
-        self.canvas.itemconfig(tk.CURRENT, fill='red')
 
-        # self.labelBox.delete(self.marker_dict[marker[0]])
-        # self.labelBox.insert(self.marker_dict[marker[0]], '({},{}) MAYBE'.format(x, y))
-        self.editMenu.post(event.x_root, event.y_root)
-
-    def addLabel(self, event):
         # Specify size of marker
         rad = 5
-
         x1, y1 = (event.x - rad), (event.y - rad)
         x2, y2 = (event.x + rad), (event.y + rad)
 
-        # Create horizontal line
-        # self.canvas.create_line(x1, event.y, x2, event.y, fill='blue')
-        # Create vertical line
-        # self.canvas.create_line(event.x, y1, event.x, y2, fill='blue')
+        closest_marker = self.canvas.find_closest(event.x, event.y)
 
-        ## Create dot
-        marker_tag = self.canvas.create_oval(x1, y1, x2, y2, fill='blue')
-        self.marker_dict[marker_tag] = self.labelBox.size()
-        print(self.marker_dict)
+        if self.currentMode.get() == 0:
+            # Remove annotation from canvas
+            self.canvas.delete(closest_marker)
 
+            # Remove annotation from visible list
+            self.coordList.delete(self.marker_dict[closest_marker[0]])
+
+            # Remove entry from backend dictionary
+            self.marker_dict.pop(closest_marker[0])
+
+
+        elif self.currentMode.get() == 1:
+
+            # If the closest marker is the canvas itself, add red
+            if closest_marker[0] == 1:
+                marker = self.canvas.create_oval(x1, y1, x2, y2, fill='red')
+                self.addCoords(event, marker)
+            # if in the space of the dot, make red
+            else:
+                self.canvas.itemconfig(closest_marker, fill='red')
+        else:
+            # Create dot
+            marker = self.canvas.create_oval(x1, y1, x2, y2, fill='blue')
+            self.addCoords(event, marker)
+
+    def addCoords(self, event, marker):
         # Find coordinates in image and print to table
         canvas = event.widget
         x = canvas.canvasx(event.x)
         y = canvas.canvasy(event.y)
-        self.labelBox.insert(tk.END, '({},{})'.format(x,y))
+        label_str = '{},{}'.format(x,y)
+        if self.currentMode.get() == 1:
+            label_str += ' (maybe)'
+        self.marker_dict[marker] = self.coordList.size()
+        self.coordList.insert(tk.END, label_str)
+
+        print(self.marker_dict)
+
 
 
     def save(self):
