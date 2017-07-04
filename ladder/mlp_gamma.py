1fee5ba50c645a0ab23e92cd92c4c44a022d9a96
Saki Shinoda <shinodasaki@gmail.com>
2017-07-04 21:06:32 +0100

    Refactor encoder

--- ladder/mlp_gamma.py
+++ ladder/mlp_gamma.py
@@ -4,8 +4,10 @@ from collections import OrderedDict
 
 class NoisyBNLayer(object):
 
-    def __init__(self, scope_name, size, decay=0.99, var_ep=1e-5):
+    def __init__(self, scope_name, size, noise_sd=None, decay=0.99, var_ep=1e-5):
         self.scope_name = scope_name
+        self.size = size
+        self.noise_sd = noise_sd
         self.decay = decay
         self.var_ep = var_ep
         with tf.variable_scope(scope_name, reuse=False):
@@ -31,42 +33,47 @@ class NoisyBNLayer(object):
         else:
             return tf.divide(tf.subtract(x, self.pop_mean), tf.add(tf.sqrt(self.pop_var), eps))
 
+    def add_noise(self, x):
+        if self.noise_sd is not None:
+            noise = tf.random_normal(shape=(BATCH_SIZE, self.size), mean=0.0, stddev=self.noise_sd)
+            return x + noise
+        else
+            return x
 
-    def apply(self, x, training=True, noise=None, scale=True):
-        x_normed = self.normalize(x, training=training)
-        if noise is not None:
-            x_normed += noise
-
+    def apply_shift_scale(self, x, shift=True, scale=True):
+        if shift:
+            x = tf.add(x, self.beta)
         if scale:
-            return tf.multiply(self.scale, tf.add(x_normed, self.beta))
-        else:
-            return tf.add(x_normed, self.beta)
+            x = tf.multiply(self.scale, x)
+        return x
 
 
 class Encoder(object):
-    def __init__(self, scope, x, y, layer_sizes, noise_sd=None):
-        self.scope = scope
+    def __init__(self, x, y, layer_sizes, noise_sd=None, reuse=None):
+        """
+
+        :param scope:
+        :param x:
+        :param y:
+        :param layer_sizes:
+        :param noise_sd: only a single scalar for all levels at this point
+        """
+        self.scope = 'enc'
+        self.reuse = reuse
         self.n_layers = len(layer_sizes)
-        self.layer_sizes = {i: layer_sizes[i] for i in range(self.n_layers)}
+        self.layer_sizes = layer_sizes
 
         self.x = x
-        self.z_pre = OrderedDict()
-        self.z = OrderedDict()
-        self.h = OrderedDict()
+        self.z_pre = list()
+        self.z = list()
+        self.h = list()
         self.y = y
 
-        if noise_sd is None:
-            self.noise = {i: None for i in range(self.n_layers)}
-            self.h[0] = x
-        else:
-            self.noise = {i: tf.random_normal(shape=(BATCH_SIZE, layer_sizes[i]), mean=0.0, stddev=noise_sd)
-                          for i in range(self.n_layers)}
-            self.h[0] = x + self.noise[0]
-
-        self.bn_layers = {i: NoisyBNLayer(scope + str(i), enc_layers[i]) for i in range(self.n_layers)}
+        self.bn_layers = [NoisyBNLayer(scope_name=self.scope+'_bn'+str(i),
+                                       size=layer_sizes[i],
+                                       noise_sd=noise_sd) for i in range(self.n_layers)]
 
         self.s_cost = None
-
 
         self.wts_init = layers.xavier_initializer()
         self.bias_init = tf.truncated_normal_initializer(stddev=1e-6)
@@ -86,25 +93,28 @@ class Encoder(object):
                 weights_regularizer=None,
                 biases_initializer=self.bias_init,
                 biases_regularizer=None,
-                reuse=None,
+                reuse=self.reuse,
                 variables_collections=None,
                 outputs_collections=None,
                 trainable=True,
-                scope=self.scope + str(l)
+                scope=self.scope+str(l)
             )
 
             if l == self.n_layers - 1:
-                self.z[l] = bn.apply(x=self.z_pre[l], training=training, noise=noise[l], scale=True)
-                self.h[l] = tf.nn.softmax_cross_entropy_with_logits(
-                    labels=self.y,
-                    logits=self.z[l])
+                self.z[l] = bn.add_noise(bn.normalize(self.z_pre[l], training))
+                self.h[l] = tf.nn.softmax(logits=bn.apply_shift_scale(self.z[l], shift=True, scale=True))
             else:
-                self.z[l] = bn.apply(x=self.z_pre[l], training=training, noise=self.noise[l], scale=False)
-                self.h[l] = tf.nn.relu(self.z[l])
+                # Do not need to apply scaling to RELU
+                self.z[l] = bn.add_noise(bn.normalize(self.z_pre[l], training))
+                self.h[l] = tf.nn.relu(bn.apply_shift_scale(self.z[l], shift=True, scale=False))
 
-        self.s_cost = self.h[-1]
+    def compute_supervised_cost(self):
+        self.s_cost = tf.nn.softmax_cross_entropy_with_logits(labels=self.y, logits=self.z[self.n_layers-1])
 
 
+# ===========================
+# PARAMETERS
+# ===========================
 BATCH_SIZE = 100
 INPUT_SIZE = 784
 TRAIN_FLAG = True
@@ -113,7 +123,6 @@ OUTPUT_SIZE = 10
 # ===========================
 # ENCODER
 # ===========================
-
 # Start with a tuple specifying layer sizes
 enc_layers = [INPUT_SIZE, 1000, 500, 250, 250, 250, OUTPUT_SIZE]
 
@@ -122,26 +131,26 @@ x = tf.placeholder(tf.float32, shape=(BATCH_SIZE, INPUT_SIZE))
 # One-hot targets
 y = tf.placeholder(tf.float32, shape=(BATCH_SIZE, OUTPUT_SIZE))
 
-# ===========================
+
 # CLEAN ENCODER
-clean_encoder = Encoder('clean', x, y, enc_layers, noise_sd=None)
+clean_encoder = Encoder(x, y, enc_layers, noise_sd=None, reuse=None)
 clean_encoder.forward_pass(TRAIN_FLAG)
 
-# ===========================
+
 # CORRUPTED ENCODER
-noisy_encoder = Encoder('corrupted', x, y, enc_layers, noise_sd=0.3)
+noisy_encoder = Encoder(x, y, enc_layers, noise_sd=0.3, reuse=True)
 noisy_encoder.forward_pass(TRAIN_FLAG)
 
+# Do we share batch norm weights
 
 # ===========================
+# GAMMA DECODER
 # ===========================
-# DECODER
-# ===========================
-# ===========================
+u_L = noisy_encoder.h[-1]
+z_L = noisy_encoder.z[-1]
 
-class DecoderLayer(object):
-    def __init__(self, input_size, output_size):
-        
+
+
 
 
 
