a468935d2b274ac3ce94a38ab1af7ee423a4fbf8
Saki Shinoda <shinodasaki@gmail.com>
2017-07-31 20:43:52 +0100

    Update CLI
    * Commented out all the CLI's not used
    * Added option to write a description
    * Description and trained log will be saved to a subdir of logs

--- vat_ladder/vat_ladder.py
+++ vat_ladder/vat_ladder.py
@@ -48,26 +48,26 @@ def parse_argstring(argstring, dtype=float, sep='-'):
 def get_cli_params():
     parser = argparse.ArgumentParser()
     parser.add_argument('--id', default='ladder')
-    parser.add_argument('--train_flag', action='store_true')
+    # parser.add_argument('--train_flag', action='store_true')
     parser.add_argument('--decay_start_epoch', default=100, type=int)
     parser.add_argument('--end_epoch', default=150, type=int)
-    parser.add_argument('--print_interval', default=50, type=int)
-    parser.add_argument('--save_epochs', default=None, type=float)
+    # parser.add_argument('--print_interval', default=50, type=int)
+    # parser.add_argument('--save_epochs', default=None, type=float)
     parser.add_argument('--num_labeled', default=100, type=int)
 
     parser.add_argument('--labeled_batch_size', default=100, type=int)
-    parser.add_argument('--unlabeled_batch_size', default=250, type=int)
+    # parser.add_argument('--unlabeled_batch_size', default=250, type=int)
 
     parser.add_argument('--initial_learning_rate', default=0.002, type=float)
 
-    parser.add_argument('--gamma_flag', action='store_true')
+    # parser.add_argument('--gamma_flag', action='store_true')
 
     # Specify encoder layers
     parser.add_argument('--encoder_layers',
                         default='784-1000-500-250-250-250-10')
 
     # Weight to apply to supervised cost in total loss
-    parser.add_argument('--sc_weight', default=1, type=float)
+    # parser.add_argument('--sc_weight', default=1, type=float)
 
     # Standard deviation of the Gaussian noise to inject at each level
     parser.add_argument('--encoder_noise_sd', default=0.3, type=float)
@@ -76,7 +76,7 @@ def get_cli_params():
     parser.add_argument('--rc_weights', default='2000-20-0.2-0.2-0.2-0.2-0.2')
 
     # Specify form of combinator (A)MLP
-    parser.add_argument('--combinator_layers', default='4-1')
+    # parser.add_argument('--combinator_layers', default='4-1')
     parser.add_argument('--combinator_sd', default=0.025, type=float)
 
     parser.add_argument('--which_gpu', default=0, type=int)
@@ -84,7 +84,7 @@ def get_cli_params():
     parser.add_argument('--seed', default=1, type=int)
 
     # by default use the unlabeled batch epochs
-    parser.add_argument('--use_labeled_epochs', action='store_true')
+    # parser.add_argument('--use_labeled_epochs', action='store_true')
 
     # only used if train_flag is false
     parser.add_argument('--train_step', default=None, type=int)
@@ -96,9 +96,12 @@ def get_cli_params():
     # weight of vat cost
     parser.add_argument('--vat_weight', default=0.1, type=float)
 
+    # description to print
+    parser.add_argument('--description', default=None)
+
     params = parser.parse_args()
-    params.write_to = 'logs/' + params.id + '.results' if params.write_to is \
-                                                        None else params.write_to
+    # params.write_to = 'logs/' + params.id + '.results' if params.write_to is None else params.write_to
+
     return params
 
 def process_cli_params(params):
@@ -518,8 +521,8 @@ i_iter = 0
 
 # -----------------------------
 # Resume from checkpoint
-
-ckpt = tf.train.get_checkpoint_state('checkpoints/')  # get latest checkpoint (if any)
+ckpt_dir = "checkpoints/" + PARAMS.id
+ckpt = tf.train.get_checkpoint_state(ckpt_dir)  # get latest checkpoint (if any)
 if ckpt and ckpt.model_checkpoint_path:
     # if checkpoint exists, restore the parameters and set epoch_n and i_iter
     saver.restore(sess, ckpt.model_checkpoint_path)
@@ -528,10 +531,25 @@ if ckpt and ckpt.model_checkpoint_path:
     print("Restored Epoch ", epoch_n)
 else:
     # no checkpoint exists. create checkpoints directory if it does not exist.
-    if not os.path.exists('checkpoints'):
-        os.makedirs('checkpoints')
+    if not os.path.exists(ckpt_dir):
+        os.makedirs(ckpt_dir)
     init = tf.global_variables_initializer()
     sess.run(init)
+
+# -----------------------------
+# Write logs to appropriate directory
+log_dir = "logs/" + PARAMS.id
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+
+if PARAMS.description is not None:
+    desc_file = log_dir + "/" + "description"
+    with open(desc_file, 'a') as f:
+        print(PARAMS.description, file=desc_file, flush=True)
+
+log_file = log_dir + "/" + "train_log"
+
+
 
 # -----------------------------
 print("=== Training ===")
@@ -568,7 +586,7 @@ for i in tqdm(range(i_iter, num_iter)):
         saver.save(sess, 'checkpoints/model.ckpt', epoch_n)
         # print "Epoch ", epoch_n, ", Accuracy: ", sess.run(accuracy, feed_dict={inputs: mnist.test.images, outputs:mnist.test.labels, training: False}), "%"
 
-        with open('train_log', 'a') as train_log:
+        with open(log_file, 'a') as train_log:
             # write test accuracy to file "train_log"
             # train_log_w = csv.writer(train_log)
             log_i = [epoch_n] + sess.run(
