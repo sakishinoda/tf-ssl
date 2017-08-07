99715cb8aeb109ea05f65ea9107c9b038f92e879
Saki Shinoda <shinodasaki@gmail.com>
2017-08-07 09:20:52 +0100

    Update queue scripts

--- vat_ladder/q0.sh
+++ vat_ladder/q0.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
-# GPU 0
-#source activate blaze
-python /home/sshinoda/tf-ssl/vat_ladder/vat_ladder.py --id "GaussVatRC_seed-11" --seed 11 --vat_weight 1.0 --ent_weight 0.0 --which_gpu 0 --vat_rc --rc_weights 1000.0-10.0-0.10-0.10-0.10-0.10-0.10
 
-python /home/sshinoda/tf-ssl/vat_ladder/vat_ladder.py --id "GaussVatRC_seed-111" --seed 111 --vat_weight 1.0 --ent_weight 0.0 --which_gpu 0 --vat_rc --rc_weights 1000.0-10.0-0.10-0.10-0.10-0.10-0.10 
+# GPU 0
+
+python /home/sshinoda/tf-ssl/vat_ladder/vat_ladder.py --id "GaussVatCorr_seed-1" --seed 1 --vat_weight 0.0 --ent_weight 0.0 --which_gpu 0 --vat_corr --rc_weights 1000.0-10.0-0.10-0.10-0.10-0.10-0.10 --test_frequency_in_epochs 5
+
+python /home/sshinoda/tf-ssl/vat_ladder/vat_ladder.py --id "GaussVatCorr_seed-11" --seed 11 --vat_weight 0.0 --ent_weight 0.0 --which_gpu 0 --vat_corr --rc_weights 1000.0-10.0-0.10-0.10-0.10-0.10-0.10 --test_frequency_in_epochs 5
+
+python /home/sshinoda/tf-ssl/vat_ladder/vat_ladder.py --id "GaussVatCorr_seed-100" --seed 100 --vat_weight 0.0 --ent_weight 0.0 --which_gpu 0 --vat_corr --rc_weights 1000.0-10.0-0.10-0.10-0.10-0.10-0.10 --test_frequency_in_epochs 5
+
+
--- vat_ladder/q1.sh
+++ vat_ladder/q1.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
-# GaussVatCorr
-python /home/sshinoda/tf-ssl/vat_ladder/vat_ladder.py --id "GaussVatCorr_seed-1" --description "VAT perturbation applied at each activation level to form the corrupted encoder. No overall VAT cost applied" --vat_weight 0.0 --ent_weight 0.0 --which_gpu 1 --vat_corr --rc_weights 1000.0-10.0-0.10-0.10-0.10-0.10-0.10
+# GPU 1
 
-python /home/sshinoda/tf-ssl/vat_ladder/vat_ladder.py --id "GaussVatCorr_seed-100" --seed 100 --vat_weight 0.0 --ent_weight 0.0 --which_gpu 1 --vat_corr --rc_weights 1000.0-10.0-0.10-0.10-0.10-0.10-0.10
+python /home/sshinoda/tf-ssl/vat_ladder/vat_ladder.py --id "GaussVatCorr_seed-111" --seed 111 --vat_weight 0.0 --ent_weight 0.0 --which_gpu 0 --vat_corr --rc_weights 1000.0-10.0-0.10-0.10-0.10-0.10-0.10 --test_frequency_in_epochs 5
 
-python /home/sshinoda/tf-ssl/vat_ladder/vat_ladder.py --id "GaussVatCorr_seed-1111" --seed 1111 --vat_weight 0.0 --ent_weight 0.0 --which_gpu 1 --vat_corr --rc_weights 1000.0-10.0-0.10-0.10-0.10-0.10-0.10
-
-
+python /home/sshinoda/tf-ssl/vat_ladder/vat_ladder.py --id "GaussVatCorr_seed-1111" --seed 1111 --vat_weight 0.0 --ent_weight 0.0 --which_gpu 0 --vat_corr --rc_weights 1000.0-10.0-0.10-0.10-0.10-0.10-0.10 --test_frequency_in_epochs 5
