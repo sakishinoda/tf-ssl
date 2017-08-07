6b565c4ae6dcb75232dfc034c7f9d97e7a842e34
Saki Shinoda <shinodasaki@gmail.com>
2017-08-07 11:29:38 +0100

    Fix q1 to use gpu 1

--- vat_ladder/q1.sh
+++ vat_ladder/q1.sh
@@ -2,12 +2,12 @@
 
 # GPU 1
 
-python vat_ladder.py --id "GaussVatCostCorrEntMin_seed-1" --seed 1 --vat_weight 1.0 --ent_weight 1.0 --which_gpu 0 --vat_corr --rc_weights 1000.0-10.0-0.10-0.10-0.10-0.10-0.10
+python vat_ladder.py --id "GaussVatCostCorrEntMin_seed-1" --seed 1 --vat_weight 1.0 --ent_weight 1.0 --which_gpu 1 --vat_corr --rc_weights 1000.0-10.0-0.10-0.10-0.10-0.10-0.10
 
-python vat_ladder.py --id "GaussVatCostCorrEntMin_seed-100" --seed 100 --vat_weight 1.0 --ent_weight 1.0 --which_gpu 0 --vat_corr --rc_weights 1000.0-10.0-0.10-0.10-0.10-0.10-0.10
+python vat_ladder.py --id "GaussVatCostCorrEntMin_seed-100" --seed 100 --vat_weight 1.0 --ent_weight 1.0 --which_gpu 1 --vat_corr --rc_weights 1000.0-10.0-0.10-0.10-0.10-0.10-0.10
 
-python vat_ladder.py --id "GaussVatCostCorrEntMin_seed-11" --seed 11 --vat_weight 1.0 --ent_weight 1.0 --which_gpu 0 --vat_corr --rc_weights 1000.0-10.0-0.10-0.10-0.10-0.10-0.10
+python vat_ladder.py --id "GaussVatCostCorrEntMin_seed-11" --seed 11 --vat_weight 1.0 --ent_weight 1.0 --which_gpu 1 --vat_corr --rc_weights 1000.0-10.0-0.10-0.10-0.10-0.10-0.10
 
-python vat_ladder.py --id "GaussVatCostCorrEntMin_seed-111" --seed 111 --vat_weight 1.0 --ent_weight 1.0 --which_gpu 0 --vat_corr --rc_weights 1000.0-10.0-0.10-0.10-0.10-0.10-0.10
+python vat_ladder.py --id "GaussVatCostCorrEntMin_seed-111" --seed 111 --vat_weight 1.0 --ent_weight 1.0 --which_gpu 1 --vat_corr --rc_weights 1000.0-10.0-0.10-0.10-0.10-0.10-0.10
 
-python vat_ladder.py --id "GaussVatCostCorrEntMin_seed-1111" --seed 1111 --vat_weight 1.0 --ent_weight 1.0 --which_gpu 0 --vat_corr --rc_weights 1000.0-10.0-0.10-0.10-0.10-0.10-0.10
+python vat_ladder.py --id "GaussVatCostCorrEntMin_seed-1111" --seed 1111 --vat_weight 1.0 --ent_weight 1.0 --which_gpu 1 --vat_corr --rc_weights 1000.0-10.0-0.10-0.10-0.10-0.10-0.10
