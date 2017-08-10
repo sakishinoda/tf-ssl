a4d8ce8eebabe5d4d8bddda30f30c1e699d2c170
Saki Shinoda <shinodasaki@gmail.com>
2017-08-10 18:59:40 +0100

    Update logging for vat mlp

--- vat/train_semisup_mnist.py
+++ vat/train_semisup_mnist.py
@@ -12,6 +12,7 @@ import time
 
 parser = argparse.ArgumentParser()
 
+parser.add_argument('--id', default='VAT')
 parser.add_argument('--which_gpu', default='0')
 parser.add_argument('--log_dir', default='')
 parser.add_argument('--seed', default=1)
@@ -23,14 +24,14 @@ parser.add_argument('--eval_freq', default=5)
 parser.add_argument('--num_epochs', default=120)
 parser.add_argument('--method', default='vat') # 'vat', 'vatent', 'baseline'
 
-FLAGS = parser.parse_args()
+params = parser.parse_args()
 
 
-FLAGS.epoch_decay_start = 80
-FLAGS.num_iter_per_epoch = 400
-FLAGS.learning_rate = 0.001
-FLAGS.mom1 = 0.9
-FLAGS.mom2 = 0.5
+params.epoch_decay_start = 80
+params.num_iter_per_epoch = 400
+params.learning_rate = 0.001
+params.mom1 = 0.9
+params.mom2 = 0.5
 
 # IPython.embed()
 
@@ -47,16 +48,16 @@ def build_training_graph(x, y, ul_x, lr, mom):
     # IPython.embed()
     nll_loss = L.ce_loss(logit, y)
     with tf.variable_scope(tf.get_variable_scope(), reuse=True):
-        if FLAGS.method == 'vat':
+        if params.method == 'vat':
             ul_logit = vat.forward(ul_x, is_training=True, update_batch_stats=False)
             vat_loss = vat.virtual_adversarial_loss(ul_x, ul_logit)
             additional_loss = vat_loss
-        elif FLAGS.method == 'vatent':
+        elif params.method == 'vatent':
             ul_logit = vat.forward(ul_x, is_training=True, update_batch_stats=False)
             vat_loss = vat.virtual_adversarial_loss(ul_x, ul_logit)
             ent_loss = L.entropy_y_x(ul_logit)
             additional_loss = vat_loss + ent_loss
-        elif FLAGS.method == 'baseline':
+        elif params.method == 'baseline':
             additional_loss = 0
         else:
             raise NotImplementedError
@@ -75,30 +76,28 @@ def accuracy(x, y):
 
 
 def main():
-    np.random.seed(FLAGS.seed)
-    tf.set_random_seed(FLAGS.seed)
+    np.random.seed(params.seed)
+    tf.set_random_seed(params.seed)
 
     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-    os.environ["CUDA_VISIBLE_DEVICES"] = str(FLAGS.which_gpu)
-
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(params.which_gpu)
 
     mnist = read_data_sets('MNIST_data', one_hot=True, n_labeled=100)
 
-
     # Training
     training_placeholders = dict(
-        inputs = tf.placeholder(tf.float32, shape=(FLAGS.batch_size, 784)),
-        outputs = tf.placeholder(tf.float32, shape=(FLAGS.batch_size, 10)),
-        ul_inputs = tf.placeholder(tf.float32, shape=(FLAGS.ul_batch_size, 784))
+        inputs = tf.placeholder(tf.float32, shape=(params.batch_size, 784)),
+        outputs = tf.placeholder(tf.float32, shape=(params.batch_size, 10)),
+        ul_inputs = tf.placeholder(tf.float32, shape=(params.ul_batch_size, 784))
 
     )
     eval_placeholders = dict(
-        inputs=tf.placeholder(tf.float32, shape=(FLAGS.eval_batch_size, 784)),
-        outputs=tf.placeholder(tf.float32, shape=(FLAGS.eval_batch_size, 10))
+        inputs=tf.placeholder(tf.float32, shape=(params.eval_batch_size, 784)),
+        outputs=tf.placeholder(tf.float32, shape=(params.eval_batch_size, 10))
     )
 
-    lr = tf.placeholder_with_default(FLAGS.learning_rate, shape=[], name="learning_rate")
-    mom = tf.placeholder_with_default(FLAGS.mom1, shape=[], name="momentum")
+    lr = tf.placeholder_with_default(params.learning_rate, shape=[], name="learning_rate")
+    mom = tf.placeholder_with_default(params.mom1, shape=[], name="momentum")
 
     with tf.variable_scope("MLP") as scope:
         loss, train_op, global_step = build_training_graph(
@@ -114,22 +113,31 @@ def main():
 
     init_op = tf.global_variables_initializer()
 
+    # -----------------------------
+    # Write logs to appropriate directory
+    log_dir = "logs/" + params.id
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+    log_file = log_dir + "/" + "train_log"
+    acc_file = log_dir + "/" + "acc_log"
+
+
     with tf.Session() as sess:
         sess.run(init_op)
-        for ep in range(FLAGS.num_epochs):
-            if ep < FLAGS.epoch_decay_start:
-                feed_dict = {lr: FLAGS.learning_rate, mom: FLAGS.mom1}
+        for ep in range(params.num_epochs):
+            if ep < params.epoch_decay_start:
+                feed_dict = {lr: params.learning_rate, mom: params.mom1}
             else:
-                decayed_lr = ((FLAGS.num_epochs - ep) / float(
-                    FLAGS.num_epochs - FLAGS.epoch_decay_start)) * FLAGS.learning_rate
-                feed_dict = {lr: decayed_lr, mom: FLAGS.mom2}
+                decayed_lr = ((params.num_epochs - ep) / float(
+                    params.num_epochs - params.epoch_decay_start)) * params.learning_rate
+                feed_dict = {lr: decayed_lr, mom: params.mom2}
 
             sum_loss = 0
             start = time.time()
 
             # TRAINING
-            for i in range(FLAGS.num_iter_per_epoch):
-                images, labels, ul_images = mnist.train.next_batch(FLAGS.batch_size, FLAGS.ul_batch_size)
+            for i in range(params.num_iter_per_epoch):
+                images, labels, ul_images = mnist.train.next_batch(params.batch_size, params.ul_batch_size)
                 feed_dict[training_placeholders['inputs']] = images
                 feed_dict[training_placeholders['outputs']] = labels
                 feed_dict[training_placeholders['ul_inputs']] = ul_images
@@ -138,22 +146,25 @@ def main():
                 sum_loss += batch_loss
 
             end = time.time()
-
-            print("Epoch:", ep, "CE_loss_train:",
-                  sum_loss / FLAGS.num_iter_per_epoch, "elapsed_time:", end - start)
+            with open(log_file, 'a') as train_log:
+                print("Epoch:", ep, "CE_loss_train:",
+                      sum_loss / params.num_iter_per_epoch, "elapsed_time:",
+                      end - start, file=train_log, flush=True)
 
             # EVAL
-            if (ep + 1) % FLAGS.eval_freq == 0 or ep + 1 == FLAGS.num_epochs:
-                # Eval on training data
+            if (ep + 1) % params.eval_freq == 0 or ep + 1 == params.num_epochs:
 
+                eval_acc = dict(train = 0, test = 0)
+
+                # Eval on training data
                 n_iter_per_epoch = mnist.train_eval.num_examples // \
-                                   FLAGS.eval_batch_size
-                eval_acc = 0
+                                   params.eval_batch_size
+
 
                 for i in range(n_iter_per_epoch):
 
                     test_images, test_labels = \
-                        mnist.test.next_batch(FLAGS.eval_batch_size)
+                        mnist.test.next_batch(params.eval_batch_size)
 
                     eval_feed_dict = {
                         eval_placeholders['inputs']: test_images,
@@ -162,21 +173,18 @@ def main():
 
                     acc_val = sess.run(acc_op, eval_feed_dict)
 
-                    eval_acc += acc_val
+                    eval_acc['train'] += acc_val
 
-                print('Train accuracy: {:4.4f}'.format(
-                    eval_acc/n_iter_per_epoch))
 
                 # Eval on testing data
 
                 n_iter_per_epoch = mnist.test.num_examples // \
-                                   FLAGS.eval_batch_size
-                eval_acc = 0
+                                   params.eval_batch_size
 
                 for i in range(n_iter_per_epoch):
 
                     test_images, test_labels = \
-                        mnist.test.next_batch(FLAGS.eval_batch_size)
+                        mnist.test.next_batch(params.eval_batch_size)
 
                     eval_feed_dict = {
                         eval_placeholders['inputs']: test_images,
@@ -185,9 +193,11 @@ def main():
 
                     acc_val = sess.run(acc_op, eval_feed_dict)
 
-                    eval_acc += acc_val
+                    eval_acc['test'] += acc_val
 
-                print('Test accuracy: {:4.4f}'.format(eval_acc/n_iter_per_epoch))
+                with open(acc_file, 'a') as acc_log:
+                    print(ep, eval_acc['train'], eval_acc['test'], file=acc_log)
+
 
 if __name__ == "__main__" :
     main()
