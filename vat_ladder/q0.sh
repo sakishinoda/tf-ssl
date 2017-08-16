384dad8877f35d8d4ff2025056e72ef35e9ba6b1
Saki Shinoda <shinodasaki@gmail.com>
2017-08-16 20:07:14 +0100

    Fix q0 for lrelu

--- vat_ladder/q0.sh
+++ vat_ladder/q0.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # GPU 0
-python mlp_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1--id "MlpLadder_act-lrelu_seed-8340" --seed 8340 --test_frequency_in_epochs 5 --which_gpu 2 --logdir "logs/MlpLadder/" --combinator "gauss"
-python mlp_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1--id "MlpLadder_act-lrelu_seed-8794" --seed 8794 --test_frequency_in_epochs 5 --which_gpu 2 --logdir "logs/MlpLadder/" --combinator "gauss"
-python mlp_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1--id "MlpLadder_act-lrelu_seed-2773" --seed 2773 --test_frequency_in_epochs 5 --which_gpu 2 --logdir "logs/MlpLadder/" --combinator "gauss"
-python mlp_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1--id "MlpLadder_act-lrelu_seed-967" --seed 967 --test_frequency_in_epochs 5 --which_gpu 2 --logdir "logs/MlpLadder/" --combinator "gauss"
-python mlp_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1--id "MlpLadder_act-lrelu_seed-2368" --seed 2368 --test_frequency_in_epochs 5 --which_gpu 2 --logdir "logs/MlpLadder/" --combinator "gauss"
+python mlp_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --id "MlpLadder_act-lrelu_seed-8340" --seed 8340 --test_frequency_in_epochs 5 --which_gpu 0 --logdir "logs/MlpLadder/" --combinator "gauss"
+python mlp_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --id "MlpLadder_act-lrelu_seed-8794" --seed 8794 --test_frequency_in_epochs 5 --which_gpu 0 --logdir "logs/MlpLadder/" --combinator "gauss"
+python mlp_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --id "MlpLadder_act-lrelu_seed-2773" --seed 2773 --test_frequency_in_epochs 5 --which_gpu 0 --logdir "logs/MlpLadder/" --combinator "gauss"
+python mlp_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --id "MlpLadder_act-lrelu_seed-967" --seed 967 --test_frequency_in_epochs 5 --which_gpu 0 --logdir "logs/MlpLadder/" --combinator "gauss"
+python mlp_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --id "MlpLadder_act-lrelu_seed-2368" --seed 2368 --test_frequency_in_epochs 5 --which_gpu 0 --logdir "logs/MlpLadder/" --combinator "gauss"
--- vat_ladder/q1.sh
+++ vat_ladder/q1.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
 # GPU 1
-python mlp_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --id "MlpLadder_cmb-mlp_seed-2368" --seed 2368 --test_frequency_in_epochs 5 --which_gpu 1 --logdir "logs/MlpLadderCmb/" --combinator "mlp"
