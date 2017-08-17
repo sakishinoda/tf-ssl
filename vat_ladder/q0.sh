a0d78091db95e48b7cc1f4b84e82cdfea74dc91a
Saki Shinoda (dopey-prism) <saki.shinoda.16@ucl.ac.uk>
2017-08-17 18:37:46 +0100

    Update queues, gitignores for vat

--- vat_ladder/q0.sh
+++ vat_ladder/q0.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # GPU 0
-python mlp_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --id "MlpLadder_seed-8340" --seed 8340 --test_frequency_in_epochs 5 --which_gpu 0 --logdir "logs/MlpLadder/"
-python mlp_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --id "MlpLadder_seed-8794" --seed 8794 --test_frequency_in_epochs 5 --which_gpu 0 --logdir "logs/MlpLadder/"
-python mlp_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --id "MlpLadder_seed-2773" --seed 2773 --test_frequency_in_epochs 5 --which_gpu 0 --logdir "logs/MlpLadder/"
-python mlp_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --id "MlpLadder_seed-967" --seed 967 --test_frequency_in_epochs 5 --which_gpu 0 --logdir "logs/MlpLadder/"
-python mlp_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --id "MlpLadder_seed-2368" --seed 2368 --test_frequency_in_epochs 5 --which_gpu 0 --logdir "logs/MlpLadder/"
+python mlp_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --id "LrDecayEveryEpoch_seed-8340" --seed 8340 --test_frequency_in_epochs 5 --which_gpu 0 --logdir "logs/MlpLadder/"
+python mlp_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --id "LrDecayEveryEpoch_seed-8794" --seed 8794 --test_frequency_in_epochs 5 --which_gpu 0 --logdir "logs/MlpLadder/"
+python mlp_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --id "LrDecayEveryEpoch_seed-2773" --seed 2773 --test_frequency_in_epochs 5 --which_gpu 0 --logdir "logs/MlpLadder/"
+python mlp_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --id "LrDecayEveryEpoch_seed-967" --seed 967 --test_frequency_in_epochs 5 --which_gpu 0 --logdir "logs/MlpLadder/"
+python mlp_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --id "LrDecayEveryEpoch_seed-2368" --seed 2368 --test_frequency_in_epochs 5 --which_gpu 0 --logdir "logs/MlpLadder/"
