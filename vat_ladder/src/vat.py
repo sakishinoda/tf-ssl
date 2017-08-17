vat_ladder: Try to fix None gradient
* Encountered this problem with VAT only previously
* Fixed by changing reduction indices of normalize_vector
* tf.stop_gradient still has Nones for AT cost
* Need to test if this is the case in the original code too
* I wonder if it has to do with the slice operation,
  i.e. if I slice after the gradient...?

vat_ladder/src/vat.py
@@ -25,13 +25,24 @@ def entropy_y_x(logit):
     return -tf.reduce_mean(tf.reduce_sum(p * logsoftmax(logit), 1))
 
 def get_normalized_vector(d):
-
-    d_dims = len(d.get_shape()) - 1
-    axes = [range(1, d_dims)] if d_dims > 1 else [1]
-    d /= (1e-12 + tf.reduce_max(tf.abs(d), axis=axes, keep_dims=True))
-    d /= tf.sqrt(1e-6 + tf.reduce_sum(tf.pow(d, 2.0), axis=axes,
+    red_axes = list(range(1, len(d.get_shape())))
+    # print(d.get_shape(), red_axes)
+    d /= (1e-12 + tf.reduce_max(tf.abs(d), axis=red_axes,
+                                            keep_dims=True))
+    d /= tf.sqrt(1e-6 + tf.reduce_sum(tf.pow(d, 2.0),
+                                      axis=red_axes,
                                       keep_dims=True))
     return d
 
 
+# def get_normalized_vector(d):
+#
+#     d_dims = len(d.get_shape()) - 1
+#     axes = [range(1, d_dims)] if d_dims > 1 else [1]
+#     d /= (1e-12 + tf.reduce_max(tf.abs(d), axis=axes, keep_dims=True))
+#     d /= tf.sqrt(1e-6 + tf.reduce_sum(tf.pow(d, 2.0), axis=axes,
+#                                       keep_dims=True))
+#     return d
+#
+#
 

vat_ladder/vat_ladder_new.py
@@ -69,6 +69,7 @@ def main():
 
         return vatfw.logits  # logits by default includes both labeled/unlabeled
 
+
     def generate_virtual_adversarial_perturbation(x, logit, is_training,
                                                   start_layer=0):
         d = tf.random_normal(shape=tf.shape(x))
@@ -101,10 +102,14 @@ def main():
     def virtual_adversarial_loss(x, logit, is_training,
                                  start_layer=0,
                                  name="vat_loss"):
+
+        print("=== VAT Pass: Generating VAT perturbation ===")
         r_vadv = generate_virtual_adversarial_perturbation(
             x, logit, is_training=is_training)
         logit = tf.stop_gradient(logit)
         logit_p = logit
+
+        print("=== VAT Pass: Computing VAT Loss (KL Divergence)")
         logit_m = forward(x + r_vadv, update_batch_stats=False,
                           is_training=is_training,
                           start_layer=start_layer)
@@ -113,12 +118,13 @@ def main():
 
 
     # AT on labeled only
-    at_cost = adversarial_loss(x=labeled(inputs),
-                               y=outputs,
-                               loss=ladder.cost,
-                               is_training=train_flag,
-                               start_layer=0
-                               ) * params.at_weight
+    # at_cost = adversarial_loss(x=labeled(inputs),
+    #                            y=outputs,
+    #                            loss=ladder.cost,
+    #                            is_training=train_flag,
+    #                            start_layer=0
+    #                            ) * params.at_weight
+    at_cost = tf.zeros(shape=tf.shape(labeled(inputs)))
 
     # VAT on unlabeled only
     vat_cost = virtual_adversarial_loss(x=unlabeled(inputs),