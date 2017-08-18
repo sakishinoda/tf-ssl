ff2931f74442f57396d887371d6ebe911d3af09a
Saki Shinoda (dopey-prism) <saki.shinoda.16@ucl.ac.uk>
2017-08-18 09:33:35 +0100

    mlp_ladder: Update queues

--- vat_ladder/q1.sh
+++ vat_ladder/q1.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
 # GPU 1
-python mlp_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --id "MlpLadder_seed-1" --seed 1 --test_frequency_in_epochs 5 --which_gpu 1 --logdir "logs/MlpLadder/"
-python mlp_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --id "MlpLadder_seed-100" --seed 100 --test_frequency_in_epochs 5 --which_gpu 1 --logdir "logs/MlpLadder/"
-python mlp_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --id "MlpLadder_seed-11" --seed 11 --test_frequency_in_epochs 5 --which_gpu 1 --logdir "logs/MlpLadder/"
-python mlp_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --id "MlpLadder_seed-111" --seed 111 --test_frequency_in_epochs 5 --which_gpu 1 --logdir "logs/MlpLadder/"
-python mlp_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --id "MlpLadder_seed-1111" --seed 1111 --test_frequency_in_epochs 5 --which_gpu 1 --logdir "logs/MlpLadder/"
+python mlp_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --id "DynamicBn_seed-967" --seed 967 --test_frequency_in_epochs 5 --which_gpu 1 --logdir "logs/MlpLadder/" --bn_decay "dynamic"
+python mlp_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --id "DynamicBn_seed-2368" --seed 2368 --test_frequency_in_epochs 5 --which_gpu 1 --logdir "logs/MlpLadder/" --bn_decay "dynamic"
