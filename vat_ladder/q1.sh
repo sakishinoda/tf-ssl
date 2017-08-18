63eac49af44244f32ffe8bc83150f17fd21c451d
Saki Shinoda <shinodasaki@gmail.com>
2017-08-18 10:20:57 +0100

    vat_ladder: Prep q1 to run VAT (no AT)

--- vat_ladder/q1.sh
+++ vat_ladder/q1.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 # GPU 1
-python mlp_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --id "DynamicBn_seed-967" --seed 967 --test_frequency_in_epochs 5 --which_gpu 1 --logdir "logs/MlpLadder/" --bn_decay "dynamic"
-python mlp_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --id "DynamicBn_seed-2368" --seed 2368 --test_frequency_in_epochs 5 --which_gpu 1 --logdir "logs/MlpLadder/" --bn_decay "dynamic"
+python vat_ladder_new.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --vat_weight 1.0 --id "vat-1.0_seed-8340" --seed 8340 --test_frequency_in_epochs 5 --lr_decay_frequency 5 --which_gpu 1 --logdir "logs/VLadder/" --bn_decay "constant"
+python vat_ladder_new.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --vat_weight 1.0 --id "vat-1.0_seed-8794" --seed 8794 --test_frequency_in_epochs 5 --lr_decay_frequency 5 --which_gpu 1 --logdir "logs/VLadder/" --bn_decay "constant"
+python vat_ladder_new.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --vat_weight 1.0 --id "vat-1.0_seed-2773" --seed 2773 --test_frequency_in_epochs 5 --lr_decay_frequency 5 --which_gpu 1 --logdir "logs/VLadder/" --bn_decay "constant"
+python vat_ladder_new.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --vat_weight 1.0 --id "vat-1.0_seed-967" --seed 967 --test_frequency_in_epochs 5 --lr_decay_frequency 5 --which_gpu 1 --logdir "logs/VLadder/" --bn_decay "constant"
+python vat_ladder_new.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --vat_weight 1.0 --id "vat-1.0_seed-2368" --seed 2368 --test_frequency_in_epochs 5 --lr_decay_frequency 5 --which_gpu 1 --logdir "logs/VLadder/" --bn_decay "constant"
