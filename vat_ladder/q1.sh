32b99266483f0ed999925199850b59af1e4bc0c9
Saki Shinoda <shinodasaki@gmail.com>
2017-08-18 18:53:26 +0100

    VAL: create queue with more parameters from VAT

--- vat_ladder/q1.sh
+++ vat_ladder/q1.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # GPU 1
-python vat_ladder_new.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --vat_weight 1.0 --id "vat-1.0_seed-8340" --seed 8340 --test_frequency_in_epochs 5 --lr_decay_frequency 5 --which_gpu 1 --logdir "logs/VLadder/" --bn_decay "constant"
-python vat_ladder_new.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --vat_weight 1.0 --id "vat-1.0_seed-8794" --seed 8794 --test_frequency_in_epochs 5 --lr_decay_frequency 5 --which_gpu 1 --logdir "logs/VLadder/" --bn_decay "constant"
-python vat_ladder_new.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --vat_weight 1.0 --id "vat-1.0_seed-2773" --seed 2773 --test_frequency_in_epochs 5 --lr_decay_frequency 5 --which_gpu 1 --logdir "logs/VLadder/" --bn_decay "constant"
-python vat_ladder_new.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --vat_weight 1.0 --id "vat-1.0_seed-967" --seed 967 --test_frequency_in_epochs 5 --lr_decay_frequency 5 --which_gpu 1 --logdir "logs/VLadder/" --bn_decay "constant"
-python vat_ladder_new.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --vat_weight 1.0 --id "vat-1.0_seed-2368" --seed 2368 --test_frequency_in_epochs 5 --lr_decay_frequency 5 --which_gpu 1 --logdir "logs/VLadder/" --bn_decay "constant"
+python vat_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --vat_weight 1.0 --id "vat-1.0_epsilon-5.0_seed-8340" --seed 8340 --test_frequency_in_epochs 10 --lr_decay_frequency 5 --which_gpu 1 --logdir "logs/VLadder/" --static_bn --epsilon 5.0 --end_epoch 400 --decay_start 0.5 --beta1 0.5
+python vat_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --vat_weight 1.0 --id "vat-1.0_epsilon-5.0_seed-8794" --seed 8794 --test_frequency_in_epochs 10 --lr_decay_frequency 5 --which_gpu 1 --logdir "logs/VLadder/" --static_bn --epsilon 5.0 --end_epoch 400 --decay_start 0.5 --beta1 0.5
+python vat_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --vat_weight 1.0 --id "vat-1.0_epsilon-5.0_seed-2773" --seed 2773 --test_frequency_in_epochs 10 --lr_decay_frequency 5 --which_gpu 1 --logdir "logs/VLadder/" --static_bn --epsilon 5.0 --end_epoch 400 --decay_start 0.5 --beta1 0.5
+python vat_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --vat_weight 1.0 --id "vat-1.0_epsilon-5.0_seed-967" --seed 967 --test_frequency_in_epochs 10 --lr_decay_frequency 5 --which_gpu 1 --logdir "logs/VLadder/" --static_bn --epsilon 5.0 --end_epoch 400 --decay_start 0.5 --beta1 0.5
+python vat_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --vat_weight 1.0 --id "vat-1.0_epsilon-5.0_seed-2368" --seed 2368 --test_frequency_in_epochs 10 --lr_decay_frequency 5 --which_gpu 1 --logdir "logs/VLadder/" --static_bn --epsilon 5.0 --end_epoch 400 --decay_start 0.5 --beta1 0.5
