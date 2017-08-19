f555f34284c61590a9df5d0ab4b152d436c84d18
Saki Shinoda <shinodasaki@gmail.com>
2017-08-19 13:02:43 +0100

    VAL: Create new single-run queues for new default lw hyperparameters

--- vat_ladder/q0.sh
+++ vat_ladder/q0.sh
@@ -2,9 +2,4 @@
 
 # GPU 0
 
-python mlp_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --id "DynamicBn_LrDecay-5_seed-1" --seed 1 --test_frequency_in_epochs 5 --lr_decay_frequency 5 --which_gpu 0 --logdir "logs/MlpLadder/" --bn_decay "dynamic"
-python mlp_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --id "DynamicBn_LrDecay-5_seed-100" --seed 100 --test_frequency_in_epochs 5 --lr_decay_frequency 5 --which_gpu 0 --logdir "logs/MlpLadder/" --bn_decay "dynamic"
-python mlp_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --id "DynamicBn_LrDecay-5_seed-11" --seed 11 --test_frequency_in_epochs 5 --lr_decay_frequency 5 --which_gpu 0 --logdir "logs/MlpLadder/" --bn_decay "dynamic"
-python mlp_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --id "DynamicBn_LrDecay-5_seed-111" --seed 111 --test_frequency_in_epochs 5 --lr_decay_frequency 5 --which_gpu 0 --logdir "logs/MlpLadder/" --bn_decay "dynamic"
-python mlp_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --id "DynamicBn_LrDecay-5_seed-1111" --seed 1111 --test_frequency_in_epochs 5 --lr_decay_frequency 5 --which_gpu 0 --logdir "logs/MlpLadder/" --bn_decay "dynamic"
-
+python vat_ladder.py --batch_size 100 --beta1 0.9 --beta1_during_decay 0.5 --decay_start 0.5 --encoder_layers 784-1000-500-250-250-250-10 --encoder_noise_sd 0.3 --end_epoch 200 --id lwval_seed-8340 --initial_learning_rate 0.002 --logdir logs/LWVAL/ --lr_decay_frequency 10 --lw 5.0-0.5-0.05-0.05-0.05-0.05-0.05 --num_labeled 100 --num_power_iterations 1 --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --seed 8340 --static_bn --test_frequency_in_epochs 10 --ul_batch_size 250 --validation --which_gpu 0
--- vat_ladder/q1.sh
+++ vat_ladder/q1.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 # GPU 1
-python vat_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --vat_weight 1.0 --id "vat-1.0_epsilon-5.0_seed-8340" --seed 8340 --test_frequency_in_epochs 10 --lr_decay_frequency 5 --which_gpu 1 --logdir "logs/VLadder/" --static_bn --epsilon 5.0 --end_epoch 400 --decay_start 0.5 --beta1 0.5
-python vat_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --vat_weight 1.0 --id "vat-1.0_epsilon-5.0_seed-8794" --seed 8794 --test_frequency_in_epochs 10 --lr_decay_frequency 5 --which_gpu 1 --logdir "logs/VLadder/" --static_bn --epsilon 5.0 --end_epoch 400 --decay_start 0.5 --beta1 0.5
-python vat_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --vat_weight 1.0 --id "vat-1.0_epsilon-5.0_seed-2773" --seed 2773 --test_frequency_in_epochs 10 --lr_decay_frequency 5 --which_gpu 1 --logdir "logs/VLadder/" --static_bn --epsilon 5.0 --end_epoch 400 --decay_start 0.5 --beta1 0.5
-python vat_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --vat_weight 1.0 --id "vat-1.0_epsilon-5.0_seed-967" --seed 967 --test_frequency_in_epochs 10 --lr_decay_frequency 5 --which_gpu 1 --logdir "logs/VLadder/" --static_bn --epsilon 5.0 --end_epoch 400 --decay_start 0.5 --beta1 0.5
-python vat_ladder.py --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --vat_weight 1.0 --id "vat-1.0_epsilon-5.0_seed-2368" --seed 2368 --test_frequency_in_epochs 10 --lr_decay_frequency 5 --which_gpu 1 --logdir "logs/VLadder/" --static_bn --epsilon 5.0 --end_epoch 400 --decay_start 0.5 --beta1 0.5
+
+python vat_ladder.py --batch_size 100 --beta1 0.9 --beta1_during_decay 0.5 --decay_start 0.5 --encoder_layers 784-1000-500-250-250-250-10 --encoder_noise_sd 0.3 --end_epoch 200 --id lwval_seed-8794 --initial_learning_rate 0.002 --logdir logs/LWVAL/ --lr_decay_frequency 10 --lw 5.0-0.5-0.05-0.05-0.05-0.05-0.05 --num_labeled 100 --num_power_iterations 1 --rc_weights 1000-10-0.1-0.1-0.1-0.1-0.1 --seed 8794 --static_bn --test_frequency_in_epochs 10 --ul_batch_size 250 --validation --which_gpu 1
+
