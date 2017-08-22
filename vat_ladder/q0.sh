839d3d7df1181f3ff9bc3395eafa7d8901f5d447
Saki Shinoda <shinodasaki@gmail.com>
2017-08-22 15:27:08 +0100

    VAT: benchmarking vat

--- vat_ladder/q0.sh
+++ vat_ladder/q0.sh
@@ -4,4 +4,4 @@
 # GPU 0
 # Previously found best Deep MLP VAT
 
-python vat_ladder.py  --batch_size 64 --beta1 0.9 --beta1_during_decay 0.5 --dataset mnist --decay_start 0.5 --do_not_save  --encoder_layers 784-1200-600-300-150-10 --end_epoch 400 --epsilon 5.0 --id DeepMlpVatMnist_seed-8340 --logdir logs/DeepMlpVatMnist/ --lr_decay_frequency 1 --model vat --seed 8340 --test_frequency_in_epochs 1 --ul_batch_size 256 --which_gpu 0
+python vat_ladder.py  --batch_size 64 --beta1 0.9 --beta1_during_decay 0.5 --dataset mnist --decay_start 0.5 --do_not_save  --encoder_layers 784-1200-600-300-150-10 --end_epoch 400 --epsilon 5.0 --id DeepMlpVatMnist_seed-8340 --logdir logs/DeepMlpVatMnist/ --lr_decay_frequency 10 --model vat --seed 8340 --static_bn 0.99 --test_frequency_in_epochs 10 --ul_batch_size 256 --vadv_sd 0.5 --which_gpu 0
