5b043d81a22f41edf63834233283e576b21bbf15
Saki Shinoda <shinodasaki@gmail.com>
2017-08-22 14:27:12 +0100

    ConvLadder: add queue to benchmark gamma conv-small on mnist

--- vat_ladder/q1.sh
+++ vat_ladder/q1.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
 # GPU 1
+python vat_ladder.py  --batch_size 100 --beta1 0.9 --beta1_during_decay 0.9 --cnn  --cnn_dims 32-32-16-16-16-8-8-8-1 --cnn_fan 1-32-32-64-64-64-128-10-10-10 --cnn_ksizes 5-2-3-2-3-1-0-0 --cnn_layer_types c-max-c-c-max-c-c-avg-fc --cnn_strides 1-2-1-1-2-1-1-0-0 --dataset mnist --decay_start 0.67 --do_not_save  --end_epoch 150 --id GammaConvSmallMnist_seed-1 --logdir logs/GammaConvSmallMnist/ --lr_decay_frequency 10 --model gamma --rc_weights 1.0 --seed 1 --test_frequency_in_epochs 10 --ul_batch_size 100 --which_gpu 1
 
-python vat_ladder.py --logdir logs/shallow/ --id shallow-nlw --model nlw --end_epoch 200 --decay_start 0.5 --which_gpu 1 --test_frequency_in_epochs 10 --encoder_layers 784-1200-1200-10 --rc_weights 1000-10-0.1-0.1 --epsilon 0.5-0.05-0.005-0.005 --lr_decay_frequency 10 --beta1 0.9 --beta1_during_decay 0.9  --num_power_iters 3
+python vat_ladder.py  --batch_size 100 --beta1 0.9 --beta1_during_decay 0.9 --cnn  --cnn_dims 32-32-16-16-16-8-8-8-1 --cnn_fan 1-32-32-64-64-64-128-10-10-10 --cnn_ksizes 5-2-3-2-3-1-0-0 --cnn_layer_types c-max-c-c-max-c-c-avg-fc --cnn_strides 1-2-1-1-2-1-1-0-0 --dataset mnist --decay_start 0.67 --do_not_save  --end_epoch 150 --id GammaConvSmallMnist_seed-11 --logdir logs/GammaConvSmallMnist/ --lr_decay_frequency 10 --model gamma --rc_weights 1.0 --seed 11 --test_frequency_in_epochs 10 --ul_batch_size 100 --which_gpu 1
 
-python vat_ladder.py --seed 1 --num_labeled 50 --batch_size 50 --ul_batch_size 250 --logdir logs/labeled-50/ --id lvan_labeled-50_seed-1 --model n --rc_weights 898.44421-8.81609-0.61101-0.11661-0.13746-0.50335-0.63461 --epsilon 0.03723 --end_epoch 200 --decay_start 0.5 --test_frequency_in_epochs 10 --lr_decay_frequency 10 --beta1 0.9 --beta1_during_decay 0.9 --which_gpu 1  --num_power_iters 3
-
-
+python vat_ladder.py  --batch_size 100 --beta1 0.9 --beta1_during_decay 0.9 --cnn  --cnn_dims 32-32-16-16-16-8-8-8-1 --cnn_fan 1-32-32-64-64-64-128-10-10-10 --cnn_ksizes 5-2-3-2-3-1-0-0 --cnn_layer_types c-max-c-c-max-c-c-avg-fc --cnn_strides 1-2-1-1-2-1-1-0-0 --dataset mnist --decay_start 0.67 --do_not_save  --end_epoch 150 --id GammaConvSmallMnist_seed-100 --logdir logs/GammaConvSmallMnist/ --lr_decay_frequency 10 --model gamma --rc_weights 1.0 --seed 100 --test_frequency_in_epochs 10 --ul_batch_size 100 --which_gpu 1
