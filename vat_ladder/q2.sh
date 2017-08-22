ac64020c8cc2e09f0b1b243e18e0905b42373543
Saki Shinoda <shinodasaki@gmail.com>
2017-08-22 14:47:05 +0100

    VAT: Benchmark in new framework

--- vat_ladder/q2.sh
+++ vat_ladder/q2.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 # GPU 2
+# Benchmark plain MLP VAT again
+# Minor difference in number of labeled/unlabeled
 
-python vat_ladder.py --logdir logs/shallow/ --id shallow-clw --model clw --end_epoch 200 --decay_start 0.5 --which_gpu 2 --test_frequency_in_epochs 10 --encoder_layers 784-1200-1200-10 --rc_weights 1000-10-0.1-0.1 --epsilon 0.5-0.05-0.005-0.005 --lr_decay_frequency 10 --beta1 0.9 --beta1_during_decay 0.9 --num_power_iters 3
+python vat_ladder.py  --batch_size 100 --beta1 0.9 --beta1_during_decay 0.5 --dataset mnist --decay_start 0.5 --do_not_save  --encoder_layers 784-1200-600-300-150-10 --end_epoch 400 --epsilon 5.0 --id DeepMlpVatMnist_seed-1 --logdir logs/DeepMlpVatMnist/ --lr_decay_frequency 10 --model vat --seed 1 --test_frequency_in_epochs 10 --ul_batch_size 250 --which_gpu 2
 
-python vat_ladder.py --seed 1 --num_labeled 50 --batch_size 50 --ul_batch_size 250 --logdir logs/labeled-50/ --id vat_labeled-50_seed-1 --model vat --epsilon 0.5 --end_epoch 200 --decay_start 0.5 --test_frequency_in_epochs 10 --lr_decay_frequency 10 --beta1 0.9 --beta1_during_decay 0.9 --which_gpu 1
+python vat_ladder.py  --batch_size 100 --beta1 0.9 --beta1_during_decay 0.5 --dataset mnist --decay_start 0.5 --do_not_save  --encoder_layers 784-1200-600-300-150-10 --end_epoch 400 --epsilon 5.0 --id DeepMlpVatMnist_seed-11 --logdir logs/DeepMlpVatMnist/ --lr_decay_frequency 10 --model vat --seed 11 --test_frequency_in_epochs 10 --ul_batch_size 250 --which_gpu 2
 
-
+python vat_ladder.py  --batch_size 100 --beta1 0.9 --beta1_during_decay 0.5 --dataset mnist --decay_start 0.5 --do_not_save  --encoder_layers 784-1200-600-300-150-10 --end_epoch 400 --epsilon 5.0 --id DeepMlpVatMnist_seed-100 --logdir logs/DeepMlpVatMnist/ --lr_decay_frequency 10 --model vat --seed 100 --test_frequency_in_epochs 10 --ul_batch_size 250 --which_gpu 2
