48c6e08deb9131ddab281e2a5a0b7335e3fd8a2b
Saki Shinoda <shinodasaki@gmail.com>
2017-08-23 14:02:26 +0100

    VAT: Add more runs for GammaConvSmall

--- vat_ladder/q0.sh
+++ vat_ladder/q0.sh
@@ -2,6 +2,7 @@
 
 
 # GPU 0
-# Previously found best Deep MLP VAT
 
-python vat_ladder.py  --batch_size 64 --beta1 0.5 --beta1_during_decay 0.5 --dataset mnist --decay_start 0.5 --do_not_save  --encoder_layers 784-1200-600-300-150-10 --end_epoch 400 --epsilon 5.0 --id DeepMlpVatMnist_seed-8340 --logdir logs/DeepMlpVatMnist/ --lr_decay_frequency 1 --model vat --seed 8340 --static_bn 0.99 --test_frequency_in_epochs 1 --ul_batch_size 256 --vadv_sd 0.5 --which_gpu 0
+python vat_ladder.py  --batch_size 100 --beta1 0.9 --beta1_during_decay 0.9 --cnn  --cnn_dims 32-32-16-16-16-8-8-8-1 --cnn_fan 1-32-32-64-64-64-128-10-10-10 --cnn_ksizes 5-2-3-3-2-3-1-0-0 --cnn_layer_types c-max-c-c-max-c-c-avg-fc --cnn_strides 1-2-1-1-2-1-1-0-0 --dataset mnist --decay_start 0.67 --do_not_save  --end_epoch 150 --id GammaConvSmallMnist_seed-111 --logdir logs/GammaConvSmallMnist/ --lr_decay_frequency 10 --model gamma --rc_weights 1.0 --seed 111 --test_frequency_in_epochs 10 --ul_batch_size 100 --which_gpu 0
+
+python vat_ladder.py  --batch_size 100 --beta1 0.9 --beta1_during_decay 0.9 --cnn  --cnn_dims 32-32-16-16-16-8-8-8-1 --cnn_fan 1-32-32-64-64-64-128-10-10-10 --cnn_ksizes 5-2-3-3-2-3-1-0-0 --cnn_layer_types c-max-c-c-max-c-c-avg-fc --cnn_strides 1-2-1-1-2-1-1-0-0 --dataset mnist --decay_start 0.67 --do_not_save  --end_epoch 150 --id GammaConvSmallMnist_seed-1111 --logdir logs/GammaConvSmallMnist/ --lr_decay_frequency 10 --model gamma --rc_weights 1.0 --seed 1111 --test_frequency_in_epochs 10 --ul_batch_size 100 --which_gpu 0
