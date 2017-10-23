dbfc511d3fde951ab2d9f52ed45435441fc562f1
Saki Shinoda <shinodasaki@gmail.com>
2017-10-23 15:30:54 +0100

    Add config for Gamma N model

--- vat_ladder/config.py
+++ vat_ladder/config.py
@@ -2,11 +2,11 @@ from argparse import Namespace
 
 p = Namespace()
 
-p.id                =   "gamma"
+p.id                =   "gamma_n"
 p.logdir            =   "train/conv_large/"
 p.ckptdir           =   "train/conv_large/"
 p.write_to          =   "description"
-p.do_not_save       =   True
+p.do_not_save       =   False
 p.verbose           =   True
 
 p.dataset	        =	"cifar10"
@@ -24,8 +24,8 @@ p.batch_size    =   100
 p.ul_batch_size =   156
 
 p.initial_learning_rate =   0.002
-p.decay_start           =   0.5
-p.lr_decay_frequency    =   5
+p.decay_start           =   0.86
+p.lr_decay_frequency    =   1
 p.beta1                 =   0.9
 p.beta1_during_decay    =   0.9
 
@@ -42,14 +42,14 @@ p.num_power_iters	=	3
 p.xi	            =	1e-6
 p.vadv_sd	        =	0.5
 
-p.model                 =   "ladder"
+p.model                 =   "n"
 p.decoder               =   "gamma"  # gamma, full, or None
 p.measure_smoothness    =   False
 p.measure_vat           =   False
 
 p.cnn               =   True
 p.cnn_layer_types   = \
-    "c-c-c-max-c-c-c-max-c-c-c-avg"
+    "cv-cf-cf-max-cv-cf-cv-max-cv-cv-cv-avg"
 p.cnn_fan           = \
     "3-96-96-96-96-192-192-192-192-192-192-10-10"
 p.cnn_ksizes        = \
