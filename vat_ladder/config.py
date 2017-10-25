b5b9da2a92a760a72295f95863744e249cb4f5dd
Saki Shinoda <shinodasaki@gmail.com>
2017-10-25 10:46:14 +0100

    Add checks in config

--- vat_ladder/config.py
+++ vat_ladder/config.py
@@ -10,7 +10,6 @@ p.do_not_save       =   False
 p.verbose           =   True
 
 p.dataset	        =	"svhn"
-p.input_size        =   784
 
 p.test_frequency_in_epochs	=	1
 p.validation	            =	0
@@ -32,7 +31,20 @@ p.beta1_during_decay    =   0.9
 p.encoder_layers	=	"1000-500-250-250-250-10"
 p.input_noise_sd    =   0.3
 p.corrupt_sd	    =	0.3
-p.rc_weights        =   "0-0-0-0-0-0-0-0-0-0-0-0-4.0"
+p.rc_weights        =   {0: 0.0,
+                         1: 0.0,
+                         2: 0.0,
+                         3: 0.0,
+                         4: 0.0,
+                         5: 0.0,
+                         6: 0.0,
+                         7: 0.0,
+                         8: 0.0,
+                         9: 0.0,
+                         10: 0.0,
+                         11: 0.0,
+                         12: 0.0,
+                         13: 4.0}
 p.static_bn	        =	0.99
 p.lrelu_a	        =	0.1
 p.top_bn            =   True
@@ -48,11 +60,40 @@ p.measure_smoothness    =   False
 p.measure_vat           =   False
 
 p.cnn               =   True
-p.cnn_layer_types   =   "c-c-c-max-c-c-c-max-cv-c-c-avg-fc"
-p.cnn_fan           =   "3-64-64-64-64-128-128-128-128-128-128-128-128-10"
-p.cnn_ksizes        =   "3-3-3-2-3-3-3-2-3-1-1-6-1"
-p.cnn_strides       =   "1-1-1-2-1-1-1-2-1-1-1-1-1"
-p.cnn_dims          =   "32-32-32-32-16-16-16-16-8-6-6-6-1-1"
+p.cnn_layer_types   =   ['c', 'c', 'c', 'max', 'c', 'c', 'c', 'max', 'cv', 'c', 'c', 'avg', 'fc']
+p.cnn_fan           =   [64, 64, 64, 64, 128, 128, 128, 128, 128, 128, 128, 128, 10]
+p.cnn_ksizes        =   [3, 3, 3, 2, 3, 3, 3, 2, 3, 1, 1, 6, 1]
+p.cnn_strides       =   [1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1, 1]
+p.cnn_dims          =   [32, 32, 32, 32, 16, 16, 16, 16, 8, 6, 6, 6, 1, ]
+
+
+# Checks
+p_dict = vars(p)
+if p.cnn:
+    num_layers = len(p.cnn_layer_types)
+    for argname in ['cnn_layer_types', 'cnn_fan', 'cnn_ksizes', 'cnn_strides',
+                'cnn_dims']:
+        arg = p_dict[argname]
+        assert len(arg) == num_layers, "Length of {} is {} vs expected {}".format(
+            argname, len(arg), num_layers)
+        assert isinstance(arg, list), "{} is a {}".format(argname, type(arg))
+
+else:
+    assert isinstance(p.encoder_layers, list), "Encoder layer spec is not a " \
+                                               "list"
+    num_layers = len(p.encoder_layers)
+
+num_layers += 1
+if p.model != 'ladder':
+    assert isinstance(p.epsilon, dict)
+    assert len(p.epsilon.keys()) == num_layers, \
+        "{} epsilons vs expected {}".format(len(p.epsilon.keys()), num_layers)
+
+elif p.model != 'vat':
+    assert isinstance(p.rc_weights, dict)
+    assert len(p.rc_weights.keys()) == num_layers, \
+        "{} weights vs expected {}".format(len(p.rc_weights.keys()),
+                                            num_layers)
 
 
 # CIFAR10
